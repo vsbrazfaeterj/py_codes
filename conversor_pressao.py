8da73117d408e7324165ff44b7d557c74312b071
vitor braz <vsbraz@faeterj-petropolis.edu.br>
2025-04-06 21:01:09 -0300

    atualizando a class conversor_ps

--- conversor_pressao.py
+++ conversor_pressao.py
@@ -1,10 +1,14 @@
-class Conversor_ps()
+class Conversor_ps:
   def __init__(self):
    
      self.pca = 100000
-     self.psi = 14,5038
+     self.psi = 14.5038
    
   def converter_barXpsi(self, bar):
      
      result = bar * self.psi 
      return result
+
+conv = Conversor_ps()
+
+print(conv.converter_barXpsi(2), "psi")
