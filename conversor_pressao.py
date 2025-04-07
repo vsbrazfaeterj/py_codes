71f5e227672aeb2da9d0ee6047220cf7f2bcfee0
vitor braz <vsbraz@faeterj-petropolis.edu.br>
2025-04-06 22:06:26 -0300

    adicionando funcionalidades

--- conversor_pressao.py
+++ conversor_pressao.py
@@ -1,39 +1,45 @@
 class Conversor_ps:
-  def __init__(self):
+   def __init__(self):
+      self.paxpsi = 0.000145038
+      self.pa = 100000
+      self.psi = 14.5038
    
-     self.pa = 100000
-     self.psi = 14.5038
-   
-  def converter_barXpsi(self, bar):
+   def converter_barXpsi(self, bar):
      
-     result = bar * self.psi 
-     return result
+      result = bar * self.psi 
+      return result
    
-  def converter_psiXbar(self, psi):
+   def converter_psiXbar(self, psi):
       if psi != 0: 
          result = psi / self.psi
          return result
       else:
          return "divisão por zero invalida"
 
-  def converter_barXpa(self, bar):
+   def converter_barXpa(self, bar):
       
       result = bar * self.pa
       return result
   
-  def converter_paXbar(self, pa):
+   def converter_paXbar(self, pa):
      
-     if pa != 0:
-      result = pa / self.pa  
-      return result
-     else:
-        return "divisão por zero imposivel!"
+      if pa != 0:
+         result = pa / self.pa  
+         return result
+      else:
+         return "divisão por zero imposivel!"
       
+   def converter_psiXpa(self, psi):
+     
+     result = psi * (self.psi / self.pa) 
+     return result
+        
    
 conv = Conversor_ps()
 
 #teste
 print(f"{conv.converter_barXpsi(2):.4f} psi")
 print(f"{conv.converter_psiXbar(14.5038 ):.4f} bar")
-print(f"{conv.converter_barXpa(0.00030000):.4f} pa")
-print(f"{conv.converter_paXbar(30):.8f} bar")
+print(f"{conv.converter_barXpa(0.0003):.4f} pa")
+print(f"{conv.converter_paXbar(3):.15f} bar")
+print(f"{conv.converter_psiXpa(0.0001):.15f} pa")
