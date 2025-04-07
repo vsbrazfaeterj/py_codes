aa604f72af3222bc9c78350aeb64fe17e237b7f6
vitor braz <vsbraz@faeterj-petropolis.edu.br>
2025-04-06 21:18:41 -0300

    adicionando converter_psiXbar()

--- conversor_pressao.py
+++ conversor_pressao.py
@@ -8,7 +8,16 @@ class Conversor_ps:
      
      result = bar * self.psi 
      return result
+   
+  def converter_psiXbar(self, psi):
+      if psi != 0: 
+         result = psi / self.psi
+         return result
+      else:
+         return "divisão por zero invalida"
+      
 
 conv = Conversor_ps()
 
 print(conv.converter_barXpsi(2), "psi")
+print(conv.converter_psiXbar(14.5038 * 2))
