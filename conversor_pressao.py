2c412a40ae010300210edbe3c43fb66e9b5185b8
vitor braz <vsbraz@faeterj-petropolis.edu.br>
2025-04-06 22:49:19 -0300

    adicionando função de conversão

--- conversor_pressao.py
+++ conversor_pressao.py
@@ -1,8 +1,8 @@
 class Conversor_ps:
    def __init__(self):
-      self.paxpsi = 0.000145038
       self.pa = 100000
       self.psi = 14.5038
+      self.pa_por_psi = 6894.76
    
    def converter_barXpsi(self, bar):
      
@@ -29,11 +29,18 @@ class Conversor_ps:
       else:
          return "divisão por zero imposivel!"
       
+   def converter_paXpsi(self, pa):
+
+      if pa != 0:
+         result = pa / self.pa_por_psi
+         return result
+      else:
+         return "divisão por zero imposivel!"
+  
    def converter_psiXpa(self, psi):
-     
-     result = psi * (self.psi / self.pa) 
-     return result
-        
+      
+      result = psi * self.pa_por_psi
+      return result        
    
 conv = Conversor_ps()
 
@@ -41,5 +48,6 @@ conv = Conversor_ps()
 print(f"{conv.converter_barXpsi(2):.4f} psi")
 print(f"{conv.converter_psiXbar(14.5038 ):.4f} bar")
 print(f"{conv.converter_barXpa(0.0003):.4f} pa")
-print(f"{conv.converter_paXbar(3):.15f} bar")
-print(f"{conv.converter_psiXpa(0.0001):.15f} pa")
+print(f"{conv.converter_paXbar(1):.15f} bar")
+print(f"{conv.converter_paXpsi(1):.9f} psi")
+print(f"{conv.converter_psiXpa(1):.4f} pa")
